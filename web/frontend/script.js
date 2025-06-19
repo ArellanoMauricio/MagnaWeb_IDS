b619194dd960d0b941c9ba90ba357973ea4ef7c6
Somnyel <lautaro_delafuente@yahoo.com>
2025-06-19 14:23:23 +0000

    Añadido cursor custom.

--- web/frontend/script.js
+++ web/frontend/script.js
@@ -41,3 +41,50 @@ async function toggleArrows() {
     await new Promise(res => setTimeout(res, 2000))
     typeWriter("ETNIAS", 0, 250, "item4")
 }
+
+document.addEventListener('mousedown', () => {
+  document.querySelector('.mf-cursor')?.classList.add('-clicked');
+});
+
+document.addEventListener('mouseup', () => {
+  document.querySelector('.mf-cursor')?.classList.remove('-clicked');
+});
+
+const cursor = new MouseFollower({
+    el: null,
+    container: document.body,
+    className: 'mf-cursor',
+    innerClassName: 'mf-cursor-inner',
+    textClassName: 'mf-cursor-text',
+    mediaClassName: 'mf-cursor-media',
+    mediaBoxClassName: 'mf-cursor-media-box',
+    iconSvgClassName: 'mf-svgsprite',
+    iconSvgNamePrefix: '-',
+    iconSvgSrc: '',
+    dataAttr: 'cursor',
+    hiddenState: '-hidden',
+    textState: '-text',
+    iconState: '-icon',
+    activeState: '-active',
+    mediaState: '-media',
+    stateDetection: {
+        '-pointer': 'a,button',
+        '-hidden': 'iframe'
+    },
+    visible: true,
+    visibleOnState: false,
+    speed: 0.55,
+    ease: 'expo.out',
+    overwrite: true,
+    skewing: 3,
+    skewingText: 2,
+    skewingIcon: 2,
+    skewingMedia: 2,
+    skewingDelta: 0.001,
+    skewingDeltaMax: 0.15,
+    stickDelta: 0.15,
+    showTimeout: 20,
+    hideOnLeave: true,
+    hideTimeout: 300,
+    hideMediaTimeout: 300
+});
