9cdc8f08b704e1f137594967e48c5f4f5d258e0f
Mauricio <miarellano@fi.uba.ar>
2025-06-24 12:36:19 -0300

    problemas de arranque de la db solucionados y api actualizada

--- web/backend/api.js
+++ web/backend/api.js
@@ -8,19 +8,42 @@ app.use(express.json())
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "frontend", "index.html"));
-});
+})
 
 app.get("/style.css", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "frontend", "style.css"));
-});
+})
+
+app.get("/animations.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "animations.css"));
+})
+
+app.get("/source/title.png", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "source", "title.png"));
+})
 
 app.get("/script.js", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "frontend", "script.js"));
-});
+})
 
-app.get("/titulo_estilizado.png", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "frontend", "titulo_estilizado.png"));
-});
+app.get("/vanillatilt.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "vanillatilt.js"));
+})
 
+app.get("/source/back.png", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "source", "back.png"));
+})
+
+app.get("/source/favicon.ico", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "source", "favicon.ico"));
+})
+
+app.get("/personajes", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "personajes.html"));
+})
+
+app.get("/lugares", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "lugares.html"));
+})
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
